563ff39a843e6886e5562ff741da60f8377d41e2
CursedBlackCat <17520254+CursedBlackCat@users.noreply.github.com>
2025-10-25 21:08:14 +0900

    Add secret rare nya sound effect

--- salt.js
+++ salt.js
@@ -1,10 +1,12 @@
 let salt = document.getElementById("salt");
-let nyaAudio = new Audio('assets/Salt1-Perfect.mp3');;
+let nyaAudio = new Audio('assets/Salt1-Perfect.mp3');
 
 function nya(){
     if (nyaAudio.paused || nyaAudio.ended) {
-        let random = Math.floor(Math.random() * 10) + 1 // random 1-10
-        if (random <= 4){
+        let random = Math.floor(Math.random() * 100) + 1 // random 1-100
+        if (random === 1) {
+            nyaAudio = new Audio('assets/Salt1-Slide.mp3');
+        } else if (random <= 40){
             nyaAudio = new Audio('assets/Salt1-GreatGood.mp3');
         } else {
             nyaAudio = new Audio('assets/Salt1-Perfect.mp3');
